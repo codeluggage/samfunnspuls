290e6c8b812b749369b32970b35c048f11b2bec1
Matias Forbord <codeluggage@posteo.net>
2026-04-07 16:32:30 +0200

    verify errors are resolved with new snippets

--- src/app/pages/EventsPage/EventsPage.tsx
+++ src/app/pages/EventsPage/EventsPage.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 
 export default function EventsPage() {
   return (
-    <div id="main">
+    <main className={styles.main}>
       <div className={styles.intro}>
         <Heading level={1} data-size="xl">
           Red Cross Events
@@ -19,8 +19,8 @@ export default function EventsPage() {
         </Paragraph>
       </div>
 
-      <section className={styles.section} aria-labelledby="events-title">
-        <Heading id="heading-title" level={2} data-size="md">
+      <section className={styles.section}>
+        <Heading level={2} data-size="md">
           First section heading
         </Heading>
         <Paragraph>
@@ -28,12 +28,12 @@ export default function EventsPage() {
         </Paragraph>
       </section>
 
-      <section className={styles.section} aria-labelledby="section-2-title">
-        <Heading id="section-2-title" level={2} data-size="md">
+      <section className={styles.section}>
+        <Heading level={2} data-size="md">
           Second section heading
         </Heading>
         <Paragraph>Add the next building block here</Paragraph>
       </section>
-    </div>
+    </main>
   );
 }
